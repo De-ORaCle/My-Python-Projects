255eaed840b124bf844c5446792f225cd818533c
Benjamin Olapade <69037600+De-ORaCle@users.noreply.github.com>
2021-08-04 04:23:21 +0100

    Update Rock! Paper! Scissors! (Score Keeping).py

--- Rock! Paper! Scissors! (Score Keeping).py
+++ Rock! Paper! Scissors! (Score Keeping).py
@@ -16,3 +16,27 @@ import random
 bot = random.randint(1,3)                                   #Making python generate a random integer between 1-3
 score = 0                                                   #Creating a variable to store the user's score
 loop = 0                                                    #Creating a variable to store the user's total number of trials
+
+while True:                                                 #Looping the code till it is instructed to stopb
+    loop = loop + 1
+    print ("Rock! Paper! Scissors!\n")
+    userput = input('''Enter 1 for Rock!
+Enter 2 for Paper!
+Enter 3 for Scissors!\n
+''')                                                        #Collecting a simple input from the user
+
+    if userput.isdigit():                                   #Making sure the user entered a digit
+        print ("")
+    else:
+        print ("You didn't enter a number!")
+        break
+
+    if bot == 1:                                            #Assigning the bot's random numbers to variables in the real game
+        botput = "Rock!"
+    elif bot == 2:
+        botput = "Paper!"
+    elif bot == 3:
+        botput = "Scissors!"
+    else:                                                   #An impossible situation
+        print ("I think I might have a mental breakdown.")
+        break
