eff1bbbcdc22701f67b36fbc8311835ee80dde54
Benjamin Olapade <69037600+De-ORaCle@users.noreply.github.com>
2021-08-05 18:01:02 +0100

    Update Simple Interest Calculator.py

--- Simple Interest Calculator.py
+++ Simple Interest Calculator.py
@@ -3,3 +3,12 @@
 This program collects three out of the four unknown variables of simple interest and calculates the last one.
 E.g. It'll receive inputs for the simple interest, principal and rate then calculate the time.
 '''
+
+print ('''Which variable do you wish to calculate:
+1. Simple Interest
+2. Principal
+3. Rate
+4. Time
+
+- You can't find a variable without knowing three other variables.
+''')
