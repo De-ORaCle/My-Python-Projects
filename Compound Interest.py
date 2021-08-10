32598ea70927a0d70f1e03e5aef820583899f8c8
Benjamin Olapade <69037600+De-ORaCle@users.noreply.github.com>
2021-08-10 21:01:13 +0100

    Update Compound Interest.py

--- Compound Interest.py
+++ Compound Interest.py
@@ -1,1 +1,15 @@
 print("This program help you calculate the final amount of a compound interest when all the necessary parameters are given.\n")
+
+P = input("Enter the initial principal balance: ")
+if P.isdigit():
+    print()
+else:
+    print("You didn't enter a valid number.")
+    exit()
+
+r = input("Enter the annual interest rate: ")
+if r.isdigit():
+    print()
+else:
+    print("You didn't enter a valid number.")
+    exit()
