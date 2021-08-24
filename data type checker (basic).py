e70175bfe0d85cc77d68ecc492e414bfa6f2179d
Benjamin Olapade <69037600+De-ORaCle@users.noreply.github.com>
2021-08-24 13:09:29 +0100

    Update data type checker (basic).py

--- data type checker (basic).py
+++ data type checker (basic).py
@@ -1,10 +1,10 @@
-#This program receives input from a user and tell the user which basic python data type the input is.
+#This program receives input from a user and tells the user which basic python data type the input is.
 
 word = input("Enter a number or text: ")
 
-if word.isalpha():
+if word.isalpha():                                             #This line processes all alphabets inputs as strings
     print("You entered a string")
-else:
+else:                                                          #Since I couldn't find a function to differentiate integers and floats, I had to note that since alphabets have been selected and floats are not considered digits, the isdigit() function would work well.
     if word.isdigit():
         print ("You entered an integer")
     else:
